c5416b622b8f161daaa2d07f5000c5538d9f1b64
Rishabh Kanojiya <rishabh.kanojiya@sportzinteractive.net>
2022-04-22 23:36:55 +0530

    re run action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <span>temp</span>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
