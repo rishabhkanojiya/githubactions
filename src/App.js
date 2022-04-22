a7698e8718146c1d449fd25360c34cd969488237
Rishabh Kanojiya <rishabh.kanojiya@sportzinteractive.net>
2022-04-23 02:21:18 +0530

    prod test run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <span>temp</span>
+          <span>Test</span>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
